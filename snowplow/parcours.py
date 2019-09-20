11119ebc1af18d7af4aa662e4c84146ad1cd0a69
Charles Parmentier <charles.parmentier@hotmail.com>
2019-09-20 15:47:29 +0200

    Snowplow : adds default function

--- snowplow/parcours.py
+++ snowplow/parcours.py
@@ -1,12 +1,54 @@
 import numpy
+import time
 
-def parcours(list):
+def average(this_list):
+	res = 0
+
+	for house in this_list:
+		res += abs(house)
+	return res / len(this_list)
+
+def parcours(this_list):
 	'''
 		return list of sorted houses in the relevant order.
 	'''
-	return
+	# print("Distance from new : {0}".format(total_distance))
+	# print("Average for new : {0}".format(total_distance / len(this_list)))
+	return this_list
 
+
+def default_parcours(this_list):
+	'''
+		same as parcours, but not optimized (used for comparing)
+		it will just reach the nearest point
+	'''
+
+	# initialize the result with the first house
+	tmp_list = list(this_list)
+	res = []
+	x = 0
+	distance = 0
+	total_distance = 0
+
+	while len(tmp_list) > 0:
+		distance = abs(tmp_list[0] - x)
+		tmp_distance = tmp_list[0]
+		for house in tmp_list:
+			if distance > abs(house - x):
+				distance = abs(house - x)
+				tmp_distance = house
+		total_distance += abs(distance)
+		res.append(tmp_distance)
+		x = tmp_distance
+		tmp_list.remove(x)
+	print("Distance from default : {0}".format(total_distance))
+	print("Average for default : {0}".format(total_distance / len(this_list)))
+	return res
 
 randlist = numpy.random.normal(0,1000,1000)
 
-parcours(randlist)
+d_p = default_parcours(randlist)
+
+n_p = parcours(randlist)
+
+print("Is it 90% worth ? : {0}".format(average(d_p)/average(n_p)<0.9))
